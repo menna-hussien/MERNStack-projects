d81ca98fa996e53695827ec89997d04559c99f83
Menna Hussien <31513435+menna-hussien@users.noreply.github.com>
2022-05-18 18:16:51 +0200

    edit basic crud

--- 01-my-blog/my-blog-backend/controllers/articles.js
+++ 01-my-blog/my-blog-backend/controllers/articles.js
@@ -1,43 +1,50 @@
 const { StatusCodes } = require('http-status-codes');
-const articlesData = require('../article-content');
+const ArticleModel = require('../models/article');
 
 const getAllarticles = async (req, res) => {
-  res.status(StatusCodes.OK).json({ articlesData, msg: 'get all articles' });
+  const articles = await ArticleModel.find({});
+  res.status(StatusCodes.OK).json({ articles, msg: 'get all articles' });
 };
 
 const getArticle = async (req, res) => {
-  const articleID = Number(req.params.id);
-  const articleFound = await articlesData.filter(
-    (article) => article.id === articleID
-  );
+  const articleID = req.params.id;
+  //const articleFound = await articlesData.filter(
+  //  (article) => article.id === articleID
+  //);
+  const article = await ArticleModel.findOne({ _id: articleID });
   res
     .status(StatusCodes.OK)
-    .json({ articleFound, msg: 'get the article', articleID });
+    .json({ article, msg: 'get the article', articleID });
 };
 
 const postArticle = async (req, res) => {
-  const { id, name, title } = req.body;
+  const { name, title, category, content } = req.body;
+  await ArticleModel.create(req.body);
 
   res
     .status(StatusCodes.OK)
-    .json({ id: id, name: name, title: title, msg: 'posted the article' });
+    .json({ name: name, title: title, msg: 'posted the article' });
 };
 
 const updateArticle = async (req, res) => {
-  const articleID = Number(req.params.id);
+  const articleID = req.params.id;
   const { id, name, title, likesNum, dislikesNum } = req.body;
-  const article = await articlesData.filter((article) => {
-    article.id === articleID;
-  });
-  const updatedArticle = article[id];
+  const article = await ArticleModel.findOneAndUpdate(
+    { _id: articleID },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
   res.status(StatusCodes.OK).json({ article, msg: 'updated the article' });
 };
 
 const deleteArticle = async (req, res) => {
   const articleID = req.params.id;
-  const article = await articlesData.filter((article) => {
-    article.id === articleID;
-  });
+
+  const article = await ArticleModel.findOneAndDelete({ _id: articleID });
+
   res.status(StatusCodes.OK).json({ article, msg: 'deleted the article' });
 };
 
