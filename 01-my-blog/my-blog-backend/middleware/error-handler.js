65d523a5799c8f4b1099bea20e06b07ea55d5c67
Menna Hussien <31513435+menna-hussien@users.noreply.github.com>
2022-05-19 13:21:50 +0200

    added custom error

--- 01-my-blog/my-blog-backend/middleware/error-handler.js
+++ 01-my-blog/my-blog-backend/middleware/error-handler.js
@@ -1,8 +1,11 @@
 const { StatusCodes } = require('http-status-codes');
+
+//this middleware is used as a customized error handling before the last middleware layer of express...without it..express will send its default errors
+// kindly note, if u decided to throw a new error with next , it will be immedialetly sent to this middleware with a new error rather than that sent by mongoose for example
 const errorHandlerMiddleware = (err, req, res, next) => {
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     msg: `Something went wrong, failed with the following error (${err.message})`,
-    error: err,
+    errorDetails: err,
   });
 };
 
